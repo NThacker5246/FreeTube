4f364414e12d2de289377d57ecee6dc6d060864e
NThacker5246 <nytrecover@gmail.com>
2025-03-08 22:12:52 +0400

    Added video display on home page

--- index.php
+++ index.php
@@ -1,6 +1,34 @@
 <?php
-
-echo "Hello, world!";
-header("Location: ./watch.php?video=1");
-
+	$fdata = file_get_contents("./db/table.json");
+	$data = json_decode($fdata);
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FreeTube - free, open source and the best analog of YT</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="/style/legacy.css">
+</head>
+<body>
+	<div class="container">
+		<div class="header">
+			<h1 style="margin: 0px;">FreeTube</h1>
+			<p style="margin-left: 30px; font-size: 20px; margin-top: 10px;">FreeTube - free, open source and the best analog of YT <a style="margin-left: 70px;  margin-top: 0px;" href="http://github.com/NThacker5246/FreeTube">Source</a> </p>
+		</div>
+		<div class="main-videos">
+			<?php
+			for ($i=1; $i < $data->count; $i++) { 
+				echo "
+				<a href=\"/watch.php?video=$i\">
+					<div class=\"card\">
+						<img src=\"preview.png\">
+						<p>$i video</p>
+					</div>
+				</a>";
+			}
+			?>
+		</div>
+	</div>
+</body>
+</html>
