ec5770cdbba5f92626b393fc1593b6a9265cebbf
NThacker5246 <nytrecover@gmail.com>
2025-05-24 20:25:31 +0400

    New Tech Stack (CPP)

---
+++ str.h
@@ -0,0 +1,26 @@
+#pragma once
+
+void strfind(char *source, const char* text, char* &out) {
+	for (int i = 0; i < strlen(source); i++) {
+		if (source[i] == text[0]) {
+			bool res = 1;
+			for (int j = 1; j < strlen(text); j++) {
+				if (source[i + j] != text[j]) {
+					res = 0;
+					i += j;
+					break;
+				}
+			}
+			if (res) {
+				i += strlen(text) + 1;
+				int j = 0;
+				for (;;) {
+					if (source[i] == ' ') return;
+					*&out[j] = source[i];
+					i += 1;
+					j += 1;
+				}
+			}
+		}
+	}
+}
