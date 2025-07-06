70646ff44a83a99914508e3cd7a8d913b9fe6d11
NThacker5246 <nytrecover@gmail.com>
2025-07-06 03:16:27 -0400

    Added html viewer

--- Program.cs
+++ Program.cs
@@ -20,7 +20,41 @@ while(true){
 		int bytesRead = st.Read(buffer, 0, buffer.Length);
                 
                 string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                Console.WriteLine("ACK: " + message);
-                st.Write(Encoding.ASCII.GetBytes($"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 20\r\nAccept-Ranges: bytes\r\n\r\n<h1>Hello World</h1>"));
+                int end = 0;
+                bool parse = false;
+                for(int i = 5; end == 0; ++i){
+                	if(message[i] == '?') parse = true;
+                	else if(message[i] == ' ') end = i - 5;
+                } 
+                string way = message.Substring(5, end);
+                Console.WriteLine($"Way: {way}, parse: {parse}\r\n");
+                if(way == ""){
+                	way = "index.html";
+                }
+                //Console.WriteLine("ACK: " + message);
+                string result = "";
+                if(!parse){
+                	try {
+		        	using (StreamReader sr = new StreamReader("front/"+way)) {
+					string line;
+					// Read and display lines from the file until the end of
+					// the file is reached.
+					line = sr.ReadLine();
+					while (line != null)
+					{
+						Console.Write(line);
+				
+						result += line;
+						line = sr.ReadLine();
+					}
+			    	}
+                	} catch (Exception e) {
+                		st.Write(Encoding.ASCII.GetBytes($"HTTP/1.1 404 Not Found\r\n"));
+                		continue;
+                	}
+                	
+                }
+                st.Write(Encoding.ASCII.GetBytes($"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {result.Length}\r\nAccept-Ranges: bytes\r\n\r\n{result}"));
 	}
-}
+}
+
